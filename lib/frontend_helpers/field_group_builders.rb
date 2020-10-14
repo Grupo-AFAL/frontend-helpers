0c688768b833730d8850d4304277384993b40f6b
Federico Gonzalez <fedegl@hey.com>
2020-10-14 16:23:51 -0700

    Display non selected text when no file is chosen

--- lib/frontend_helpers/field_group_builders.rb
+++ lib/frontend_helpers/field_group_builders.rb
@@ -67,7 +67,15 @@ module FrontendHelpers
         :span, non_selected_text, class: 'file-name', data: { target: 'file-input.value' }
       )
 
-      @template.content_tag(:div, class: 'field file has-name', data: { controller: 'file-input' }) do
+      wrapper_options = {
+        class: 'field file has-name',
+        data: {
+          controller: 'file-input',
+          'file-input-non-selected-text': non_selected_text
+        }
+      }
+
+      @template.content_tag(:div, wrapper_options) do
         @template.content_tag(:label, class: 'file-label') do
           file_field(method, options) + file_cta + filename
         end
