a655764bf2c3bbaf319ec804defa99255df090f9
Federico Gonzalez <fedegl@hey.com>
2020-10-15 12:00:04 -0700

    Integrar ellipsis en el codigo para no depender de fontawesome

--- lib/frontend_helpers/components.rb
+++ lib/frontend_helpers/components.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 module FrontendHelpers
+  ELLIPSIS_SVG = %(
+    <svg viewBox="0 0 512 512" class="svg-inline--fa fa-ellipsis-h fa-w-16">
+      <path fill="currentColor"
+        d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72 72 32.2 72 72zm104-72c-39.8
+        0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72
+        72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z">
+      </path>
+    </svg>
+  )
+
   module Components
     def icon_tag(name, options = {})
       options.merge!(class: "icon #{options[:class]}")
@@ -10,20 +20,20 @@ module FrontendHelpers
       end
     end
 
-    def ellipsis_dropdown(html_options = {})
+    def ellipsis_dropdown(html_options = {}, &block)
       html_options.with_defaults!(class: '', data: { controller: 'dropdown' })
       html_options[:class] += ' dropdown is-right'
 
       content_tag(:div, html_options) do
         trigger = content_tag(:div, class: 'dropdown-trigger') do
           content_tag(:button, class: 'button', data: { action: 'dropdown#toggleMenu' }) do
-            content_tag(:i, ''.html_safe, class: 'fas fa-ellipsis-h')
+            ELLIPSIS_SVG.html_safe
           end
         end
 
         dropdown = content_tag(:div, class: 'dropdown-menu', role: 'menu') do
           content_tag(:div, class: 'dropdown-content') do
-            yield
+            block.call
           end
         end
 
