8b61bd2948dab62d0a6c481a892d59206fa23b6b
Federico Gonzalez <fedegl@hey.com>
2021-02-04 09:17:42 -0800

    Move Component helpers from EnjoyKitchen

--- lib/frontend_helpers/components.rb
+++ lib/frontend_helpers/components.rb
@@ -40,5 +40,31 @@ module FrontendHelpers
     def boolean_icon(value)
       value ? icon_tag('check-circle') : icon_tag('times-circle')
     end
+
+    def active_link(name, path, options = {})
+      path_without_parmms = path.split('?').first
+
+      is_active = case options[:match]
+                  when :partial
+                    request.path.include?(path)
+                  when String
+                    request.path.include?(options[:match])
+                  when Array
+                    options[:match].any? { |p| request.path.include?(p) }
+                  else
+                    path_without_parmms == request.path
+                  end
+
+      options[:class] = class_names(options[:class], 'is-active': is_active)
+      link_to name, path, options
+    end
+
+    def link_with_icon(name, icon_name, path, options = {})
+      icon = icon_tag(icon_name, class: options.delete(:icon_class))
+
+      link_to path, options do
+        icon + tag.span(name)
+      end
+    end
   end
 end
