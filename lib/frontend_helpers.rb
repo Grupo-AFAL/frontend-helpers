eb492dcbcd527b1e25a9bf98fcdec02d1fa960de
Federico Gonzalez <fedegl@hey.com>
2021-02-25 14:32:51 -0800

    Use hardcoded SVG icons and add link_to_modal

--- lib/frontend_helpers.rb
+++ lib/frontend_helpers.rb
@@ -5,6 +5,7 @@ require 'frontend_helpers/version'
 # require 'action_pack'
 
 require 'frontend_helpers/utils'
+require 'frontend_helpers/icon_components'
 require 'frontend_helpers/builder_html_utils'
 require 'frontend_helpers/shared_form_builder_utils'
 require 'frontend_helpers/field_group_wrapper'
@@ -13,11 +14,11 @@ require 'frontend_helpers/form_builder'
 require 'frontend_helpers/layout_concern'
 require 'frontend_helpers/dynamic_fields_helper'
 require 'frontend_helpers/components'
-require 'frontend_helpers/icons'
 require 'frontend_helpers/custom_redirect_concern'
 
 module FrontendHelpers
   class Engine < ::Rails::Engine; end
+
   class Error < StandardError; end
   # Your code goes here...
 end
--- lib/frontend_helpers/components.rb
+++ lib/frontend_helpers/components.rb
@@ -3,12 +3,13 @@
 module FrontendHelpers
   module Components
     include Utils
+    include IconComponents
 
     def icon_tag(name, options = {})
-      options.merge!(class: "icon #{options[:class]}")
+      options[:class] = class_names(['icon', options[:class]])
 
-      content_tag(:span, options) do
-        content_tag(:i, '', class: "fas fa-#{name}")
+      tag.span(options) do
+        icon_svgs(name)
       end
     end
 
@@ -16,11 +17,12 @@ module FrontendHelpers
       html_options.with_defaults!(class: '', data: { controller: 'dropdown' })
       html_options[:class] += ' dropdown is-right'
       button_class = class_names(['button', html_options.delete(:button_class)])
-      button_content = html_options.delete(:button_content) || Icons::ELLIPSIS_SVG.html_safe
+      button_content = html_options.delete(:button_content) || icon_svgs('ellipsis-h')
 
       content_tag(:div, html_options) do
         trigger_tag = content_tag(:div, class: 'dropdown-trigger') do
-          content_tag(:button, title: 'dropdown-button', class: button_class, data: { action: 'dropdown#toggleMenu' }) do
+          content_tag(:button, title: 'dropdown-button', class: button_class,
+                               data: { action: 'dropdown#toggleMenu' }) do
             button_content
           end
         end
@@ -65,6 +67,37 @@ module FrontendHelpers
       link_to path, options do
         icon + tag.span(name)
       end
+    end
+
+    # Convinience method to launch a link within a modal.
+    # Appends the data-action="remote-modal#open" to trigger the RemoteModalController
+    #
+    def link_to_modal(name, options = nil, html_options = nil, &block)
+      if block_given?
+        html_options = options
+        options = name
+        name = block
+      end
+      options ||= {}
+
+      html_options = convert_options_to_data_attributes(options, html_options)
+
+      url = url_for(options)
+      html_options['href'] ||= url
+      html_options['data-redirect-to'] = request.path
+      html_options['data-action'] = 'remote-modal#open'
+
+      content_tag('a', name || url, html_options, &block)
+    end
+
+    # Adds the submit-button stimulus controller so that the Submit button
+    # displays a spinner when clicked.
+    #
+    def form_with(**options, &block)
+      options[:data] ||= {}
+      options[:data][:controller] = "#{options.dig(:data, :controller)} submit-button".strip
+
+      super(**options, &block)
     end
   end
 end
