8226222cdfdbcfac6c73fe3e20e13389c30432fa
Federico Gonzalez <fedegl@hey.com>
2021-02-26 18:47:19 -0800

    Add times-circle icon

--- lib/frontend_helpers/icon_components.rb
+++ lib/frontend_helpers/icon_components.rb
@@ -294,6 +294,14 @@ module IconComponents
     </svg>
   )
 
+  TIMES_CIRCLE = %(
+    <svg viewBox="0 0 512 512" class="svg-inline">
+      <path fill="currentColor"
+        d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"
+        class=""></path>
+    </svg>
+  )
+
   MAP = {
     'arrow-left' => ARROW_LEFT,
     'arrow-right' => ARROW_RIGHT,
@@ -326,7 +334,8 @@ module IconComponents
     'coins' => COINS,
     'truck' => TRUCK,
     'user-plus' => USER_PLUS,
-    'print' => PRINT
+    'print' => PRINT,
+    'times-circle' => TIMES_CIRCLE
   }.freeze
 
   class IconNotAvailable < StandardError; end
