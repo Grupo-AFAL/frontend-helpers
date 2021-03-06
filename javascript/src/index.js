b999043711133d0ecdede8927d2480bf0feab084
Federico Gonzalez <fedegl@hey.com>
2021-03-06 12:41:16 -0800

    Use microbundle and experiment with different setups

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,50 +1,50 @@
-import * as domHelpers from './utils/domHelpers'
-import * as formatters from './utils/formatters'
-import * as form from './utils/form'
-import GoogleMapsLoader from './utils/google-maps-loader'
-import * as time from './utils/time'
-import * as url from './utils/url'
+// import * as domHelpers from './utils/domHelpers'
+// import * as formatters from './utils/formatters'
+// import * as form from './utils/form'
+// import GoogleMapsLoader from './utils/google-maps-loader'
+// import * as time from './utils/time'
+// import * as url from './utils/url'
 
-import DatepickerController from './controllers/datepicker-controller'
-import DropdownController from './controllers/dropdown-controller'
-import DynamicFieldsController from './controllers/dynamic-fields-controller'
-import LocalModalController from './controllers/local-modal-controller'
-import NavbarController from './controllers/navbar-controller'
-import NotificationController from './controllers/notification-controller'
-import PhoneInputController from './controllers/phone-input-controller'
-import RadioGroupController from './controllers/radio-group-controller'
-import RemoteModalController from './controllers/remote-modal-controller'
-import SelectFieldController from './controllers/select-field-controller'
-import SideMenuController from './controllers/side-menu-controller'
-import StepNumberInputController from './controllers/step-number-input-controller'
-import SubmitButtonController from './controllers/submit-button-controller'
-import SubmitOnChangeController from './controllers/submit-on-change-controller'
-import TabsController from './controllers/tabs-controller'
-import TooltipController from './controllers/tooltip-controller'
-import FileInputController from './controllers/file-input-controller'
+// import DatepickerController from './controllers/datepicker-controller'
+import DropdownController from './controllers/dropdown-controller.js'
+// import DynamicFieldsController from './controllers/dynamic-fields-controller'
+// import LocalModalController from './controllers/local-modal-controller'
+// import NavbarController from './controllers/navbar-controller'
+// import NotificationController from './controllers/notification-controller'
+// import PhoneInputController from './controllers/phone-input-controller'
+// import RadioGroupController from './controllers/radio-group-controller'
+// import RemoteModalController from './controllers/remote-modal-controller'
+// import SelectFieldController from './controllers/select-field-controller'
+// import SideMenuController from './controllers/side-menu-controller'
+// import StepNumberInputController from './controllers/step-number-input-controller'
+// import SubmitButtonController from './controllers/submit-button-controller'
+// import SubmitOnChangeController from './controllers/submit-on-change-controller'
+// import TabsController from './controllers/tabs-controller'
+// import TooltipController from './controllers/tooltip-controller'
+// import FileInputController from './controllers/file-input-controller'
 
 export {
-  domHelpers,
-  form,
-  formatters,
-  GoogleMapsLoader,
-  time,
-  url,
-  DatepickerController,
-  DropdownController,
-  DynamicFieldsController,
-  LocalModalController,
-  NavbarController,
-  NotificationController,
-  PhoneInputController,
-  RadioGroupController,
-  RemoteModalController,
-  SelectFieldController,
-  SideMenuController,
-  StepNumberInputController,
-  SubmitButtonController,
-  SubmitOnChangeController,
-  TabsController,
-  TooltipController,
-  FileInputController
+  // domHelpers,
+  // form,
+  // formatters,
+  // GoogleMapsLoader,
+  // time,
+  // url,
+  // DatepickerController,
+  DropdownController
+  // DynamicFieldsController,
+  // LocalModalController,
+  // NavbarController,
+  // NotificationController,
+  // PhoneInputController,
+  // RadioGroupController,
+  // RemoteModalController,
+  // SelectFieldController,
+  // SideMenuController,
+  // StepNumberInputController,
+  // SubmitButtonController,
+  // SubmitOnChangeController,
+  // TabsController,
+  // TooltipController,
+  // FileInputController
 }
