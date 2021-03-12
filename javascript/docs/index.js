6d1fa837df26e4c68081bad44d69a58424144a43
Alexis Gutiérrez <mgutierrez@afal.mx>
2021-03-12 11:24:11 -0800

    Added Hovercard Controller (#7)
    * Added hovercard controller
    
    * Add hovercard component to playground
    
    * Add Tabs component to playground
    
    * Components descriptions and autogenerate hovercard elements

--- javascript/docs/index.js
+++ javascript/docs/index.js
@@ -4,9 +4,13 @@ import { Application } from 'stimulus'
 
 import { ChartController } from '../src/controllers/chart-controller'
 import { DropdownController } from '../src/controllers/dropdown-controller'
+import { TabsController } from '../src/controllers/tabs-controller'
 import { TagsController } from '../src/controllers/tags-controller'
+import { HovercardController } from '../src/controllers/hovercard-controller'
 
 const application = Application.start()
 application.register('chart', ChartController)
 application.register('dropdown', DropdownController)
+application.register('hovercard', HovercardController)
+application.register('tabs', TabsController)
 application.register('tags', TagsController)
--- javascript/src/controllers/tabs-controller.js
+++ javascript/src/controllers/tabs-controller.js
@@ -1,29 +1,10 @@
 import { Controller } from 'stimulus'
 
-/**
- * Tabs controller
- *
- * <div data-controller="tabs">
- *   <div class="tabs">
- *     <ul>
- *       <li class="is-active" data-tab-index="1" data-action="click->tabs#open">
- *         <a>Activities</a>
- *       </li>
- *       <li data-tab-index="2" data-action="click->tabs#open">
- *         <a>Other tab</a>
- *       </li>
- *     </ul>
- *   </div>
- *   <div class="content">
- *     <div data-content-index="1">
- *       Activities tab content
- *     </div>
- *     <div data-content-index="2" class="is-hidden">
- *       Other tab content
- *     </div>
- *   </div>
- * </div>
- */
+/*
+  Tabs controller
+  It generates a tabs component with open and hide content actions
+  with active tab property.
+*/
 
 export class TabsController extends Controller {
   open (event) {
