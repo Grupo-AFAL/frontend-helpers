fde5a0a4e836eba18f540abefb18ada5327f922b
Alexis Gutierrez <mgutierrez@afal.mx>
2021-03-11 16:24:39 -0800

    Add Tabs component to playground

--- javascript/docs/index.js
+++ javascript/docs/index.js
@@ -3,10 +3,12 @@ import './index.scss'
 import { Application } from 'stimulus'
 
 import { DropdownController } from '../src/controllers/dropdown-controller'
+import { TabsController } from '../src/controllers/tabs-controller'
 import { TagsController } from '../src/controllers/tags-controller'
 import { HovercardController } from '../src/controllers/hovercard-controller'
 
 const application = Application.start()
 application.register('dropdown', DropdownController)
-application.register('tags', TagsController)
 application.register('hovercard', HovercardController)
+application.register('tabs', TabsController)
+application.register('tags', TagsController)
--- javascript/src/controllers/hovercard-controller.js
+++ javascript/src/controllers/hovercard-controller.js
@@ -1,6 +1,26 @@
 import { Controller } from 'stimulus'
 import { stringToDOMNode } from '../utils/domHelpers'
 
+/* 
+  Hovercard component:
+    <a data-controller="hovercard"
+      data-hovercard-url-value="<%= cafeteria_product_item_path(@product_item) %>"
+      class="button has-text-danger is-text" disabled>Eliminar
+    </a> 
+    
+  Hovercard content:
+    <%= hovercard do %>
+      <div class="card-content">
+        <div class="content">
+          <p>Este elemento forma parte de un producto, no puede ser eliminado</p>
+          <% @products_in_use.each do |product| %>
+            <strong><%= product.name %></strong>
+          <% end %>
+        </div>
+      </div>
+    <% end %>
+*/
+
 export class HovercardController extends Controller {
   static values = { url: String }
 
