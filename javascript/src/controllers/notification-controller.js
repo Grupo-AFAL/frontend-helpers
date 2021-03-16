f868733380621844ad0b524d4f16c0bfc4a9a2f4
Federico Gonzalez <fedegl@hey.com>
2021-03-15 21:35:38 -0700

    Add notification component with dismiss behaviour

--- javascript/src/controllers/notification-controller.js
+++ javascript/src/controllers/notification-controller.js
@@ -1,7 +1,25 @@
 import { Controller } from 'stimulus'
 
 export class NotificationController extends Controller {
-  hide () {
-    this.element.parentNode.removeChild(this.element)
+  static values = {
+    delay: Number
+  }
+
+  connect () {
+    this.closed = false
+    setTimeout(() => this.close(), this.delayValue || 3000)
+  }
+
+  disconnect () {
+    if (this.closed) return
+    this.element.remove()
+  }
+
+  close () {
+    this.element.classList.add('fadeOutRight')
+    this.element.addEventListener('animationend', () => {
+      this.closed = true
+      this.element.remove()
+    })
   }
 }
