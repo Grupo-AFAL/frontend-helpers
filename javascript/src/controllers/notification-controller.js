2267feaaccaa982bf041276a448025edb2145eac
Federico Gonzalez <fedegl@hey.com>
2021-03-15 21:45:08 -0700

    Add comment to notification controller

--- javascript/src/controllers/notification-controller.js
+++ javascript/src/controllers/notification-controller.js
@@ -1,5 +1,10 @@
 import { Controller } from 'stimulus'
 
+/**
+ * Notification Controller
+ * Automatically hides the notification message after 3 seconds by default
+ * The user can chose to close the notification by clicking the close button.
+ */
 export class NotificationController extends Controller {
   static values = {
     delay: Number
