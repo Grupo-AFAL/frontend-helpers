117b2e2b6fb7ba4db9b6cd03f73dc9eaff8fa191
Federico Gonzalez <fedegl@gmail.com>
2021-03-28 20:19:46 -0700

    Add calendar-alt icon (#29)
    * Add calendar-alt icon
    
    * Add missing changelog items
    
    Co-authored-by: Federico Gonzalez <fedegl@hey.com>

--- lib/frontend_helpers/icon_components.rb
+++ lib/frontend_helpers/icon_components.rb
@@ -318,6 +318,14 @@ module IconComponents
     </svg>
   )
 
+  CALENDAR_ALT = %(
+    <svg viewBox="0 0 448 512" class="svg-inline">
+      <path fill="currentColor"
+        d="M148 288h-40c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12zm108-12v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 96v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm192 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96-260v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h48V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h128V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h48c26.5 0 48 21.5 48 48zm-48 346V160H48v298c0 3.3 2.7 6 6 6h340c3.3 0 6-2.7 6-6z"
+        class=""></path>
+    </svg>
+  )
+
   MAP = {
     'arrow-left' => ARROW_LEFT,
     'arrow-right' => ARROW_RIGHT,
@@ -353,7 +361,8 @@ module IconComponents
     'print' => PRINT,
     'times-circle' => TIMES_CIRCLE,
     'search' => SEARCH,
-    'chevron-down' => CHEVRON_DOWN
+    'chevron-down' => CHEVRON_DOWN,
+    'calendar-alt' => CALENDAR_ALT
   }.freeze
 
   class IconNotAvailable < StandardError; end
