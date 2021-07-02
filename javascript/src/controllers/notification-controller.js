34d98b646f8e9317db4b70d98bc428ffb8a37bd3
Miguel Frias <miguel.frias@cetys.edu.mx>
2021-07-02 10:25:13 -0700

    Fix rubocop

--- javascript/src/controllers/notification-controller.js
+++ javascript/src/controllers/notification-controller.js
@@ -1,10 +1,9 @@
-import { Controller } from 'stimulus'
-import { DisappearController } from './disappear-controller';
+import { DisappearController } from './disappear-controller'
 
 /**
  * Notification Controller
  * Automatically hides the notification message after 3 seconds by default
  * The user can chose to close the notification by clicking the close button.
  */
-export class NotificationController extends  DisappearController {
+export class NotificationController extends DisappearController {
 }
