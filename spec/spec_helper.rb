ef61d07af2e1750ebdefa9d10da5369ff1e561f6
Federico Gonzalez <fedegl@hey.com>
2021-07-23 17:26:36 -0700

    Fix rspec formatter and download coverage files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov_json_formatter'
+
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::JSONFormatter
+])
 
 SimpleCov.start 'rails' do # <============= 2
   add_filter 'spec/'
