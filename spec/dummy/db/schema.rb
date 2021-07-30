4ae4b53eb43169f13452ebad351845c7c3a6a579
Brandon Matthew <matthewrc95@gmail.com>
2021-07-30 07:39:30 -0700

    Added changes for rubocop

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,37 +12,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_212951) do
-
-  create_table "characters", force: :cascade do |t|
-    t.string "name"
-    t.integer "movie_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["movie_id"], name: "index_characters_on_movie_id"
+ActiveRecord::Schema.define(version: 20_210_729_212_951) do
+  create_table 'characters', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'movie_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['movie_id'], name: 'index_characters_on_movie_id'
   end
 
-  create_table "movies", force: :cascade do |t|
-    t.string "name"
-    t.integer "status", default: 0
-    t.integer "tenant_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["tenant_id"], name: "index_movies_on_tenant_id"
+  create_table 'movies', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'status', default: 0
+    t.integer 'tenant_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['tenant_id'], name: 'index_movies_on_tenant_id'
   end
 
-  create_table "tenants", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'tenants', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "workouts", force: :cascade do |t|
-    t.integer "workout_start_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'workouts', force: :cascade do |t|
+    t.integer 'workout_start_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  add_foreign_key "characters", "movies"
-  add_foreign_key "movies", "tenants"
+  add_foreign_key 'characters', 'movies'
+  add_foreign_key 'movies', 'tenants'
 end
