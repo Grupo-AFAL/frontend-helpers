80618ba82c4a7a8381f35de5ff3d822939cfb11d
Federico Gonzalez <fedegl@gmail.com>
2021-08-03 17:43:18 -0700

    Create new ToggleController (#82)
    Co-authored-by: Federico Gonzalez <fedegl@hey.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.3.2] - 2021-08-03
+
+- Create new ToggleController to toggle content on and off based on the value of a checkbox
+
 ## [0.3.1] - 2021-08-03
 
 - Allow boolean_field to receive check_box checked and unchecked values
---
+++ javascript/src/controllers/toggle-controller.js
@@ -0,0 +1,37 @@
+import { Controller } from 'stimulus'
+
+/**
+ * Toggles ON and OFF different elements based on the state of a checkbox
+ *
+ * <div data-controller="toggle" data-toggle-checked-value="true">
+ *   <input data-action="toggle#change" type="checkbox">
+ *   <div data-toggle-target="off">
+ *     Off content
+ *   </div>
+ *  <div data-toggle-target="on">
+ *    On content
+ *  </div>
+ * </div>
+ */
+export class ToggleController extends Controller {
+  static targets = ['on', 'off']
+  static values = { checked: Boolean }
+
+  connect () {
+    this.toggleTargets(this.checkedValue)
+  }
+
+  change (event) {
+    this.toggleTargets(event.target.checked)
+  }
+
+  toggleTargets (checked) {
+    if (checked) {
+      this.onTarget.classList.remove('is-hidden')
+      this.offTarget.classList.add('is-hidden')
+    } else {
+      this.onTarget.classList.add('is-hidden')
+      this.offTarget.classList.remove('is-hidden')
+    }
+  }
+}
