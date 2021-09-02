fe9f9fea5ffc483946f340b126b1ed1a7396725a
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2021-09-02 16:00:47 -0700

    Add manual closing to notification (#100)
    * Add manual closing functionality to notification
    
    * Move functionality to notificaion
    
    * increment version
    
    * Restore file
    
    * Fix indentatio+
    
    * Fix standard js
    
    * Fix indentation
    
    * Auto closing if delay value is given, otherwise is manual close

--- javascript/src/controllers/notification-controller.js
+++ javascript/src/controllers/notification-controller.js
@@ -2,8 +2,14 @@ import { DisappearController } from './disappear-controller'
 
 /**
  * Notification Controller
- * Automatically hides the notification message after 3 seconds by default
+ * Automatically hides the notification message if delay value is given
  * The user can chose to close the notification by clicking the close button.
  */
 export class NotificationController extends DisappearController {
+  connect () {
+    this.closed = false
+    if (!this.hasDelayValue) { return }
+
+    setTimeout(() => this.close(), this.delayValue)
+  }
 }
