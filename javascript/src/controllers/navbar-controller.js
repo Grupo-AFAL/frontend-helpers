cc1c98b836c04c10cecfeeb4e175ac7c4236da05
Miguel Frias <miguel.frias@cetys.edu.mx>
2021-09-08 14:53:02 -0700

    Add toggleable on scroll class

--- javascript/src/controllers/navbar-controller.js
+++ javascript/src/controllers/navbar-controller.js
@@ -1,7 +1,26 @@
 import { Controller } from 'stimulus'
 
 export class NavbarController extends Controller {
+  static values = { scrollingThreshold: Number }
+  static classes = ['toggleableOnScroll']
   static targets = ['menu', 'burger']
+
+  connect () {
+    if (!this.scrollingThresholdValue) return
+
+    this.toggleableClass = this.hasToggleableOnScrollClass ? this.toggleableOnScrollClass : 'transparent'
+
+    this.element.classList.add(this.toggleableClass)
+    document.addEventListener('scroll', this.updateBackgroundColor)
+  }
+
+  updateBackgroundColor = () => {
+    if (window.scrollY > this.scrollingThresholdValue) {
+      this.element.classList.remove(this.toggleableClass)
+    } else {
+      this.element.classList.add(this.toggleableClass)
+    }
+  }
 
   toggleMenu (event) {
     event.preventDefault()
