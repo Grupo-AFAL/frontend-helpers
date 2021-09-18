6d8fe46546a3175b125c0f4df1287ce99e766885
Federico Gonzalez <fedegl@hey.com>
2021-09-18 15:52:07 -0700

    Fix rubocop again

--- lib/frontend_helpers/field_group_builders.rb
+++ lib/frontend_helpers/field_group_builders.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/ModuleLength
 module FrontendHelpers
+  # rubocop:disable Metrics/ModuleLength
   module FieldGroupBuilders
     include Components
 
@@ -192,5 +192,5 @@ module FrontendHelpers
       end
     end
   end
+  # rubocop:enable Metrics/ModuleLength
 end
-# rubocop:enable Metrics/ModuleLength
