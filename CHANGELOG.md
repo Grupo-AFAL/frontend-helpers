fea1b574f9223bf811219a8ac7434ebfd8b8b306
Federico Gonzalez <fedegl@gmail.com>
2021-09-19 18:37:20 -0700

    Add filter icon (#108)
    Co-authored-by: Federico Gonzalez <fedegl@hey.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.3.19] - 2021-09-19
+
+- Add filter icon
+
 ## [0.3.18] - 2021-09-18
 
 - Add search_field_group form tag builder
--- lib/frontend_helpers/icon_components.rb
+++ lib/frontend_helpers/icon_components.rb
@@ -137,6 +137,14 @@ module IconComponents
     </svg>
   )
 
+  FILTER = %(
+    <svg viewBox="0 0 512 512" class="svg-inline">
+      <path fill="currentColor"
+        d="M463.952 0H48.057C5.419 0-16.094 51.731 14.116 81.941L176 243.882V416c0 15.108 7.113 29.335 19.2 40l64 47.066c31.273 21.855 76.8 1.538 76.8-38.4V243.882L497.893 81.941C528.042 51.792 506.675 0 463.952 0zM288 224v240l-64-48V224L48 48h416L288 224z"
+        class=""></path>
+    </svg>
+  )
+
   LONG_ARROW_ALT_LEFT = %(
     <svg viewBox="0 0 448 512" class="svg-inline">
       <path fill="currentColor"
@@ -855,6 +863,7 @@ module IconComponents
     'expand' => EXPAND,
     'facebook-square' => FACEBOOK_SQUARE,
     'facebook' => FACEBOOK,
+    'filter' => FILTER,
     'fire-alt' => FIRE_ALT,
     'ga-day' => GA_DAY,
     'ga-month' => GA_MONTH,
