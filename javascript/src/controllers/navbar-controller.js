5ded3dcd84c3919ca2cef8c6be00bb4abd5d66a2
Miguel Frias <miguel.frias@cetys.edu.mx>
2021-09-29 13:25:32 -0700

    Add is-active to navbar when menu is active

--- javascript/src/controllers/navbar-controller.js
+++ javascript/src/controllers/navbar-controller.js
@@ -4,7 +4,6 @@ export class NavbarController extends Controller {
   static values = { allowTransparency: Boolean, throttleInterval: Number }
   static targets = ['menu', 'burger']
 
-  touchScreenThreshold = 1023; // bulma touch screen threshold
 
   connect () {
     if (!this.allowTransparencyValue) return
@@ -36,8 +35,7 @@ export class NavbarController extends Controller {
   }
 
   setIsTransparent () {
-    const isMenuActive = this.menuTarget.classList.contains('is-active')
-    if (this.isTransparent || (isMenuActive && this.isOnTouchDevice())) return
+    if (this.isTransparent) return
 
     this.isTransparent = true
     this.element.classList.add('is-transparent')
@@ -52,19 +50,11 @@ export class NavbarController extends Controller {
 
   toggleMenu (event) {
     event.preventDefault()
+    this.element.classList.toggle('is-active')
     this.menuTarget.classList.toggle('is-active')
 
     if (this.hasBurgerTarget) {
-      if (this.isOnTouchDevice() && window.scrollY < this.burgerTarget.offsetHeight) {
-        this.isTransparent = !this.isTransparent
-        this.element.classList.toggle('is-transparent')
-      }
-
       this.burgerTarget.classList.toggle('is-active')
     }
-  }
-
-  isOnTouchDevice () {
-    return window.innerWidth <= this.touchScreenThreshold
   }
 }
