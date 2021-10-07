ecf4d5a99f72a5670f983ba4c57c0d58a26cdf38
Federico Gonzalez <fedegl@hey.com>
2021-10-07 14:02:31 -0700

    Use lodash debounce function

--- javascript/src/controllers/submit-on-change-controller.js
+++ javascript/src/controllers/submit-on-change-controller.js
@@ -1,5 +1,6 @@
 import { Controller } from 'stimulus'
 import { submitForm } from '../utils/form'
+import debounce from 'lodash.debounce'
 
 /**
  * SubmitOnChange Controller
@@ -17,16 +18,21 @@ import { submitForm } from '../utils/form'
  * Optionally you can specify the method and responseKind as values to the controller.
  */
 export class SubmitOnChangeController extends Controller {
-  timeoutId = undefined;
-
   static values = {
     delay: Number,
     method: String,
     responseKind: String
   }
 
+  connect () {
+    if (this.hasDelayValue && this.delayValue > 0) {
+      this.submit = debounce(this.submit, this.delayValue)
+    }
+  }
+
   async submit () {
     const options = {}
+
     if (this.hasMethodValue) {
       options.method = this.hasMethodValue
     }
@@ -35,8 +41,6 @@ export class SubmitOnChangeController extends Controller {
       options.responseKind = this.responseKindValue
     }
 
-    if (this.timeoutId) { clearTimeout(this.timeoutId) }
-
-    this.timeoutId = setTimeout(() => { submitForm(this.element, options) }, this.delayValue || 0)
+    submitForm(this.element, options)
   }
 }
