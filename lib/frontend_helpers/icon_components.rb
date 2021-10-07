3676f4ecc594654c51fa09a6c4a2d50dd8850cd2
Ivan Aguilar <ivan96318@gmail.com>
2021-10-07 10:24:32 -0700

    fix mius icon and add some icons

--- lib/frontend_helpers/icon_components.rb
+++ lib/frontend_helpers/icon_components.rb
@@ -565,6 +565,12 @@ module IconComponents
     </svg>
   )
 
+  MINUS = %(
+    <svg viewBox="0 0 14 2" class="svg-inline" fill="none">
+      <path d="M0.599609 0H13.3996V1.60001H0.599609V0Z" fill="currentColor"/>
+    </svg>
+  )
+
   PHONE = %(
     <svg viewBox="0 0 12 12" class="svg-inline" fill="currentColor">
       <path d="M2.41333 5.19333C3.37333 7.08 4.92 8.62 6.80667 9.58667L8.27333 8.12C8.45333 7.94 8.72 7.88 8.95333 7.96C9.7 8.20667 10.5067 8.34 11.3333 8.34C11.7 8.34 12 8.64 12 9.00667V11.3333C12 11.7 11.7 12 11.3333 12C5.07333 12 0 6.92667 0 0.666667C0 0.3 0.3 0 0.666667 0H3C3.36667 0 3.66667 0.3 3.66667 0.666667C3.66667 1.5 3.8 2.3 4.04667 3.04667C4.12 3.28 4.06667 3.54 3.88 3.72667L2.41333 5.19333Z" fill="white"/>
@@ -843,12 +849,25 @@ module IconComponents
   )
 
   EXCLAMATION_CIRCLE = %(
-    <svg viewBox="0 0 512 512" class="svg-inline--fa fa-exclamation-circle fa-w-16 fa-2x">
+    <svg viewBox="0 0 512 512" class="svg-inline--fa">
       <path fill="currentColor" d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm42-104c0 23.159-18.841 42-42 42s-42-18.841-42-42 18.841-42 42-42 42 18.841 42 42zm-81.37-211.401l6.8 136c.319 6.387 5.591 11.401 11.985 11.401h41.17c6.394 0 11.666-5.014 11.985-11.401l6.8-136c.343-6.854-5.122-12.599-11.985-12.599h-54.77c-6.863 0-12.328 5.745-11.985 12.599z" class=""></path>
     </svg>
   )
 
+  ADDRESS_BOOK = %(
+    <svg viewBox="0 0 448 512" class="svg-inline--fa">
+      <path fill="currentColor" d="M436 160c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h320c26.5 0 48-21.5 48-48v-48h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20zm-68 304H48V48h320v416zM208 256c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm-89.6 128h179.2c12.4 0 22.4-8.6 22.4-19.2v-19.2c0-31.8-30.1-57.6-67.2-57.6-10.8 0-18.7 8-44.8 8-26.9 0-33.4-8-44.8-8-37.1 0-67.2 25.8-67.2 57.6v19.2c0 10.6 10 19.2 22.4 19.2z" class=""></path>
+    </svg>
+  )
+
+  UTENSILS_ALT = %(
+    <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="utensils-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-utensils-alt fa-w-18 fa-2x">
+      <path fill="currentColor" d="M0 74.1c0 114.2 47.3 199 156.1 223L88 356.6c-31.9 28.2-33.4 77.6-3.3 107.7l26.9 26c30.3 30.3 79.6 28.5 107.7-3.3l70.1-79.1c74 87.3 67.1 79.2 68.5 80.7 28.2 30.1 76.4 31.6 106.3 1.7l26-26c29.7-29.7 28.7-78.1-2-106.6l-59.4-55.2c25.5-3.9 50.3-16.2 71.3-37.2 20.7-20.7 58.4-74.8 66.1-85.9 23.5-33.6 1.2-78.9-35.8-91.9-6.4-18.5-22.8-35.3-42-42.1-12.8-36.3-58-59.6-91.9-35.8-11.2 7.8-65.2 45.4-85.9 66.1-23 23-36.3 51.4-38.3 81.4L124.4 19.9C76.9-24.2 0 9.9 0 74.1zm348.3 153.6c-33.6-33.6-40.1-81.6-3.7-118C363.4 90.9 424 49 424 49c7.3-5.3 24.1 11.8 18.6 18.9L365 145.5c-6.7 7.8 10.5 25.3 18.6 18.9l82.6-73.3c7.2-5.1 23.9 11.5 18.7 18.7l-73.3 82.6c-6.4 8.1 11 25.3 18.9 18.6l77.6-77.6c7.1-5.5 24.1 11.3 18.9 18.6 0 0-41.9 60.6-60.7 79.5-35.8 35.8-83.8 30.5-118-3.8zm-138.7 86.1l48.5 57.1-74.6 84.3c-9.9 11.2-27.3 11.7-37.8 1.1l-26-26c-10.6-10.6-10.1-27.9 1.1-37.8l88.8-78.7zM48 74c0-22.6 27-34.5 43.7-19l364 338c10.8 10 11.1 27 .7 37.4l-26 26c-10.4 10.4-27.3 10.1-37.4-.6L223.5 256C93.5 256 48 182.8 48 74z" class=""></path>
+    </svg>
+  )
+
   MAP = {
+    'address-book' => ADDRESS_BOOK,
     'alert' => ALERT,
     'arrow-left' => ARROW_LEFT,
     'arrow-right' => ARROW_RIGHT,
@@ -873,6 +892,7 @@ module IconComponents
     'cutlery' => CUTLERY,
     'edit' => EDIT,
     'ellipsis-h' => ELLIPSIS_H,
+    'exclamation-circle' => EXCLAMATION_CIRCLE,
     'expand' => EXPAND,
     'facebook-square' => FACEBOOK_SQUARE,
     'facebook' => FACEBOOK,
@@ -892,6 +912,7 @@ module IconComponents
     'map-marked-alt' => MAP_MARKED_ALT,
     'mail' => MAIL,
     'mexico-flag' => MEXICO_FLAG,
+    'minus' => MINUS,
     'money-bill-wave' => MONEY_BILL_WAVE,
     'mute' => MUTE,
     'nested-arrow' => NESTED_ARROW,
@@ -923,6 +944,7 @@ module IconComponents
     'us-flag' => US_FLAG,
     'user' => USER,
     'user-plus' => USER_PLUS,
+    'utensils-alt' => UTENSILS_ALT,
     'weight' => WEIGHT
   }.freeze
 
