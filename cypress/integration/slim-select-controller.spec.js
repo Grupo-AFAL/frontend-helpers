c7553bb056187a01e24790460dad7109f133859e
Ivan Aguilar <ivan96318@gmail.com>
2022-01-26 08:59:09 -0800

    add test for deslect option

--- cypress/integration/slim-select-controller.spec.js
+++ cypress/integration/slim-select-controller.spec.js
@@ -15,5 +15,25 @@ context('SlimSelectController', () => {
       .should($c => {
         expect($c).to.have.length(6)
       })
+    cy.get('#button-select-all').should('not.be.visible')
+    cy.get('#button-desselect-all').should('be.visible')
+  })
+
+  it('deselects all values', () => {
+    cy.get('#button-select-all').click()
+    cy.get('#select-all .ss-values')
+      .children()
+      .should($c => {
+        expect($c).to.have.length(6)
+      })
+
+    cy.get('#button-desselect-all').click()
+    cy.get('#select-all .ss-values')
+      .children()
+      .should($c => {
+        expect($c).to.have.length(1)
+      })
+    cy.get('#button-select-all').should('be.visible')
+    cy.get('#button-desselect-all').should('not.be.visible')
   })
 })
