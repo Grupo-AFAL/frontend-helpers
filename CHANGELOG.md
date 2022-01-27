749d4a9725d87fa1a98d679e1ed2e95210e138db
Federico Gonzalez <fedegl@hey.com>
2022-01-26 16:52:02 -0800

    Use Popper.js for hovercard positioning

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.5.7] - 2022-01-26
+
+- Use Popper.js for positioning the hovercard
+
 ## [0.5.6] - 2022-01-26
 
 - Add new properties `close_on_select` and `allow_deselect_option` to `slim_select_field` method.
--- javascript/src/controllers/hovercard-controller.js
+++ javascript/src/controllers/hovercard-controller.js
@@ -1,12 +1,8 @@
 import { Controller } from '@hotwired/stimulus'
+import { createPopper } from '@popperjs/core'
 import { stringToDOMNode } from '../utils/domHelpers'
 
-const SPACE_BETWEEN_NODE_AND_ELEMENT = 10
-const SPACE_TO_RIGHT_EDGE = 30
-const MIN_LEFT_POSITION = 8
-
-const CONTAINER_SELECTOR = '.hovercard'
-const SVG_SELECTOR = 'svg.svg-hover-card'
+const CONTENT_CLASS_NAME = 'content'
 
 /*
   Hovercard controller:
@@ -21,17 +17,25 @@ export class HovercardController extends Controller {
 
   connect () {
     this.isActive = false
+    this.contentLoaded = false
 
     this.element.addEventListener('mouseenter', this.show.bind(this))
     this.element.addEventListener('mouseleave', this.hide.bind(this))
-    document.addEventListener('scroll', () => this.removeCardNode())
-    window.addEventListener('resize', () => this.removeCardNode())
+
+    this.cardNode = this.buildCardNode(null)
+    this.element.appendChild(this.cardNode)
+
+    this.popperInstance = createPopper(this.element, this.cardNode, {
+      placement: 'top'
+    })
   }
 
   show () {
     this.isActive = true
 
-    if (this.cardNode) { return this.cardNode.classList.remove('is-hidden') }
+    if (this.contentLoaded) {
+      return this.showAndUpdateHovercardPosition()
+    }
 
     if (this.urlValue) {
       fetch(this.urlValue)
@@ -42,139 +46,60 @@ export class HovercardController extends Controller {
     }
   }
 
-  insertHoverCard (html) {
+  insertHoverCard (html = null) {
     if (!this.isActive) return
+    this.contentLoaded = true
 
-    this.cardNode = stringToDOMNode(this.buildHtml(html)).querySelector(
-      CONTAINER_SELECTOR
-    )
-    document.body.appendChild(this.cardNode)
-
-    this.positionHovercard()
+    this.cardNode.querySelector(`.${CONTENT_CLASS_NAME}`).innerHTML = html
+    this.showAndUpdateHovercardPosition()
   }
 
-  buildHtml (html) {
-    return `<div class="hovercard card">
-              <div class="card-content">
-                <div class="content">
-                  ${html}
-                </div>
-              </div>
-              <svg width="20" height="8" viewBox="0 0 20 8" fill="none" class="svg-hover-card">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M10 8C13 8 15.9999 0 20 0H0C3.9749 0 7 8 10 8Z" fill="white"></path>
-              </svg>
-            </div`
+  showAndUpdateHovercardPosition () {
+    this.cardNode.classList.remove('is-hidden')
+
+    // Enable the event listeners
+    this.popperInstance.setOptions(options => ({
+      ...options,
+      modifiers: [
+        ...options.modifiers,
+        { name: 'eventListeners', enabled: true }
+      ]
+    }))
+
+    this.popperInstance.update()
   }
 
-  positionHovercard () {
-    const svgNode = this.cardNode.querySelector(SVG_SELECTOR)
-    const {
-      hoverCardTop,
-      hoverCardLeft,
-      isDown,
-      isLeft,
-      isRight
-    } = this.hoverCardDimensions()
-
-    if (isDown) {
-      svgNode.style.bottom = this.cardNode.offsetHeight
-      svgNode.setAttribute('transform', 'rotate(180)')
-    }
-
-    const { left, width } = this.element.getBoundingClientRect()
-    const elementPadding = this.calculateInnerWidth(this.element)
-
-    if (isLeft) {
-      svgNode.style.left = `${left + (width - elementPadding) / 2}px`
-    } else if (isRight) {
-      svgNode.style.left = `${left -
-        hoverCardLeft +
-        (width - elementPadding) / 2}`
-    }
-
-    this.cardNode.setAttribute(
-      'style',
-      `top:${hoverCardTop}px; left:${hoverCardLeft}px; position:fixed`
-    )
-  }
-
-  calculateInnerWidth (element) {
-    return (
-      parseFloat(
-        window
-          .getComputedStyle(element, null)
-          .getPropertyValue('padding-right')
-          .replace('px', '')
-      ) +
-      parseFloat(
-        window
-          .getComputedStyle(element, null)
-          .getPropertyValue('padding-left')
-          .replace('px', '')
-      )
-    )
-  }
-
-  hoverCardDimensions () {
-    const { top, left, width, height } = this.element.getBoundingClientRect()
-    const {
-      height: nodeHeight,
-      width: nodeWidth
-    } = this.cardNode.getBoundingClientRect()
-
-    let isRight = false
-    let isDown = false
-    let isLeft = false
-
-    let hoverCardTop = top - nodeHeight - SPACE_BETWEEN_NODE_AND_ELEMENT
-    let hoverCardLeft = left - nodeWidth / 2 + width / 2
-
-    // check is the hovercard is near the top
-    if (hoverCardTop < 0) {
-      hoverCardTop = top + height + SPACE_BETWEEN_NODE_AND_ELEMENT
-      isDown = true
-    }
-
-    // check if hovercard is less than the minimun left position to the left
-    if (hoverCardLeft < MIN_LEFT_POSITION) {
-      hoverCardLeft = MIN_LEFT_POSITION
-      isLeft = true
-    }
-
-    // check if hovercard is near the right side
-    const minSpaceRequired = nodeWidth + SPACE_TO_RIGHT_EDGE
-    const spaceAvailable = window.innerWidth - hoverCardLeft
-
-    if (spaceAvailable < minSpaceRequired) {
-      hoverCardLeft += spaceAvailable - minSpaceRequired
-      isRight = true
-    }
-
-    return {
-      hoverCardTop,
-      hoverCardLeft,
-      isDown,
-      isLeft,
-      isRight
-    }
+  buildCardNode (html) {
+    return stringToDOMNode(
+      `<div class="hovercard card is-hidden">
+        <div class="card-content">
+          <div class="${CONTENT_CLASS_NAME}">
+            ${html || ''}
+          </div>
+        </div>
+        <svg width="20" height="8" viewBox="0 0 20 8" fill="none" class="svg-hover-card">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M10 8C13 8 15.9999 0 20 0H0C3.9749 0 7 8 10 8Z" fill="white"></path>
+        </svg>
+      </div`
+    ).firstElementChild
   }
 
   hide () {
     this.isActive = false
+    this.cardNode.classList.add('is-hidden')
 
-    if (this.cardNode) {
-      this.cardNode.classList.add('is-hidden')
-    }
+    // Disable the event listeners
+    this.popperInstance.setOptions(options => ({
+      ...options,
+      modifiers: [
+        ...options.modifiers,
+        { name: 'eventListeners', enabled: false }
+      ]
+    }))
   }
 
   disconnect () {
-    this.removeCardNode()
-  }
-
-  removeCardNode () {
-    if (this.cardNode) {
-      this.cardNode.remove()
-      this.cardNode = null
-    }
+    this.cardNode.remove()
+    this.cardNode = null
   }
 }
